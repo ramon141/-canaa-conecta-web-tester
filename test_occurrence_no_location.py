cd6e5b08acb52f6801ce04dcd580c91b902e8026
Ramon Barbosa <141barbosa@gmail.com>
2024-02-05 17:09:45 -0300

    Relatorio de ocorrencias sem localizacao com filtro

--- test_occurrence_no_location.py
+++ test_occurrence_no_location.py
@@ -10,3 +10,9 @@ def test_occurrence_no_location():
     browser = Browser()
     login(browser)
     assert complaint.no_location(browser) == 29
+
+
+def test_occurrence_no_location_with_filter():
+    browser = Browser()
+    login(browser)
+    assert complaint.no_location_with_filter(browser, 'M1502152-20240131-690882') == 1
