9eca1f80410a09718b965ab978c6a433ad32efe4
Ramon Barbosa <141barbosa@gmail.com>
2024-02-23 13:59:00 -0300

    Ajustando testes com variaveis de ambiente

--- test_occurrence_no_location.py
+++ test_occurrence_no_location.py
@@ -2,6 +2,7 @@ from browser import Browser
 from dotenv import load_dotenv
 from login import login
 import complaint
+from os import getenv
 
 load_dotenv()
 
@@ -9,10 +10,10 @@ load_dotenv()
 def test_occurrence_no_location():
     browser = Browser()
     login(browser)
-    assert complaint.no_location(browser) == 29
+    assert str(complaint.no_location(browser)) == getenv('TABLE_NO_LOCATION')
 
 
 def test_occurrence_no_location_with_filter():
     browser = Browser()
     login(browser)
-    assert complaint.no_location_with_filter(browser, 'M1502152-20240131-690882') == 1
+    assert complaint.no_location_with_filter(browser, getenv('TABLE_NO_LOCATION_FILTER')) == 1
