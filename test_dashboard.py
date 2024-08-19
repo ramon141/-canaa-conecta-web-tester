ae80336bddbc4bdb01b7f7932111f818a75bf67d
Ramon Barbosa <141barbosa@gmail.com>
2024-08-18 22:04:15 -0300

    adicionando teste de integracao

--- test_dashboard.py
+++ test_dashboard.py
@@ -3,17 +3,18 @@ from dotenv import load_dotenv
 from login import login
 from dashboard import get_quant_occurrences_attendance, get_quant_occurrences_open
 from os import getenv
+import os
 
 load_dotenv()
 
 
 def test_occurrence_attendance():
     browser = Browser()
-    login(browser)
+    login(browser, os.getenv('ADMIN_EMAIL'), os.getenv('ADMIN_PASSWORD'))
     assert get_quant_occurrences_attendance(browser) == getenv('DASHBOARD_QUANT_OCCURRENCES_ATTENDANCE')
 
 
 def test_occurrences_open():
     browser = Browser()
-    login(browser)
+    login(browser, os.getenv('ADMIN_EMAIL'), os.getenv('ADMIN_PASSWORD'))
     assert get_quant_occurrences_open(browser) == getenv('DASHBOARD_QUANT_OCCURRENCES_OPEN')
--- test_occurrence_no_location.py
+++ test_occurrence_no_location.py
@@ -3,17 +3,18 @@ from dotenv import load_dotenv
 from login import login
 import complaint
 from os import getenv
+import os
 
 load_dotenv()
 
 
 def test_occurrence_no_location():
     browser = Browser()
-    login(browser)
+    login(browser, os.getenv('ADMIN_EMAIL'), os.getenv('ADMIN_PASSWORD'))
     assert str(complaint.no_location(browser)) == getenv('TABLE_NO_LOCATION')
 
 
 def test_occurrence_no_location_with_filter():
     browser = Browser()
-    login(browser)
+    login(browser, os.getenv('ADMIN_EMAIL'), os.getenv('ADMIN_PASSWORD'))
     assert complaint.no_location_with_filter(browser, getenv('TABLE_NO_LOCATION_FILTER')) == 1
